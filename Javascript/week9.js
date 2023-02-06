fd7721b01c659cb31f4ca0e4c15ed296758eaa5e
Prapti KC <np03cs4a220384@heraldcollege.edu.np>
2023-02-06 12:56:43 +0545

    added portfolio

---
+++ Javascript/week9.js
@@ -0,0 +1,24 @@
+console.log('hello world')
+// alert("hello Section 15");
+// prompt("what is your name?");
+
+
+let x=10;
+let y=20;
+console.log(x+y);
+const color="pink";
+console.log(color);
+console.log(x+y);
+console.log(x>y);
+let num="5";
+let number =5;
+console.log(num==number);
+console.log(num===number);
+
+let name="John";
+let Name="John";
+
+//Q1 Calculate how many seconds are there in 5 hour.
+let s=5;
+seconds=5*60*60;
+console.log(seconds);
---
+++ Week 9 & 10/wnine2.js
@@ -0,0 +1,5 @@
+language = 'Indian';
+const country = 'India';
+const continent = 'Asia';
+const isIsland = false;
+isIsland = true;
